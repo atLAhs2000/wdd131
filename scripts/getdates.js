c2a9cb86ac6af9c22d3ad71e5ed5f0c4d1c80999
atLAhs2000 <alyssalahoy@gmail.com>
2024-05-09 20:38:47 -0700

    finish last of css and javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,11 @@
 //select all document elements
-const currentYear = document.getElementById("#current-year");
+const currentYear = document.getElementById("currentyear");
+const modified = document.lastModified;
+const lastModified = document.getElementById("lastModified");
 
 //use DATE object
 const today = new Date();
 
 //set innerHTML of elements
 currentYear.innerHTML = today.getFullYear();
+lastModified.innerHTML = `Last Modification: ${modified}`;
